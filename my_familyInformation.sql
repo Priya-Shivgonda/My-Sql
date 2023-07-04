6712f478d985527fba15fccbc252dfcd07b319e5
Priya-Shivgonda <138101085+Priya-Shivgonda@users.noreply.github.com>
2023-07-04 10:53:54 +0530

    my_familyInformation.sql

--- my_familyInformation.sql
+++ my_familyInformation.sql
@@ -11,6 +11,7 @@ CREATE TABLE  my_father(surname varchar(20),
                         pan_no varchar(10),
                         name varchar(34),
                         age int );
+DESC my_father;
                         
                         
 CREATE TABLE  my_mother(surname varchar(20),
@@ -23,7 +24,7 @@ CREATE TABLE  my_mother(surname varchar(20),
                         pan_no varchar(10),
                         name varchar(34),
                         age int );
-                        
+DESC my_brother;
                         
 CREATE TABLE  my_sister(surname varchar(20),
 	                mobileNo bigint,
@@ -35,7 +36,7 @@ CREATE TABLE  my_sister(surname varchar(20),
                         pan_no varchar(10),
                         name varchar(34),
                         age int);
-                        
+     DESC   my_sister;                   
 		
   CREATE TABLE  my_brother(surname varchar(20),
 			mobileNo bigint,
@@ -46,7 +47,8 @@ CREATE TABLE  my_sister(surname varchar(20),
                         adhar_no bigint,
                         pan_no varchar(10),
                         name varchar(34),
-                        age int );                      
+                        age int ); 
+DESC my_brother;
 CREATE TABLE  my_uncle(surname varchar(20),
 			mobileNo bigint,
                         gmail varchar(50),
@@ -57,7 +59,7 @@ CREATE TABLE  my_uncle(surname varchar(20),
                         pan_no varchar(10),
                         name varchar(34),
                         age int );  
-                        
+   DESC my_uncle;                    
  CREATE TABLE  my_unty(surname varchar(20),
 			mobileNo bigint,
                         gmail varchar(50),
@@ -68,6 +70,7 @@ CREATE TABLE  my_uncle(surname varchar(20),
                         pan_no varchar(10),
                         name varchar(34),
                         age int );  
+DESC my_unty;
 CREATE TABLE  my_grandfather(surname varchar(20),
 			mobileNo bigint,
                         gmail varchar(50),
@@ -78,6 +81,7 @@ CREATE TABLE  my_grandfather(surname varchar(20),
                         pan_no varchar(10),
                         name varchar(34),
                         age int );
+DESC my_grandfather;
 CREATE TABLE  my_grandmother(surname varchar(20),
 			mobileNo bigint,
                         gmail varchar(50),
@@ -88,6 +92,7 @@ CREATE TABLE  my_grandmother(surname varchar(20),
                         pan_no varchar(10),
                         name varchar(34),
                         age int );
+DESC my_grandmother;
 CREATE TABLE  my_sisteinlaw(surname varchar(20),
 			mobileNo bigint,
                         gmail varchar(50),
@@ -98,6 +103,7 @@ CREATE TABLE  my_sisteinlaw(surname varchar(20),
                         pan_no varchar(10),
                         name varchar(34),
                         age int );
+DESC my_sister;
 CREATE TABLE  my_nephew(surname varchar(20),
 			mobileNo bigint,
                         gmail varchar(50),
@@ -108,9 +114,7 @@ CREATE TABLE  my_nephew(surname varchar(20),
                         pan_no varchar(10),
                         name varchar(34),
                         age int);
-                        
-	desc my_father;
-    
+DESC my_nephew;    
     
                         
 
